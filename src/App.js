1ae2e617471699a5233db3a898f14134eada6914
Roger Takeshita <roger.takeshita@gmail.com>
2020-03-14 03:38:36 -0400

    update App.js and style css
    - App.js - separated privet routes vs public routes
    - Created a folder to centralize all css
    - Updated paths (index.js and app.js) to the new css path
    - Converted css file into SASS

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import './App.css';
+import './css/App.css';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import SignupPage from './pages/SignupPage/SignupPage';
 import NewApplicationPage from './pages/NewApplicationPage/NewApplicationPage';
+import userService from './utils/userService';
 
 function App(props) {
+    let pages = userService.getUser() ? (
+        <Switch>
+            <Route
+                exact
+                path="/new"
+                render={({ history }) => (
+                    <NewApplicationPage history={history} />
+                )}
+            />
+            <Route exact path="/" render={() => <HomePage />} />
+            <Route render={() => <Redirect to={{ pathname: '/' }} />} />
+        </Switch>
+    ) : (
+        <Switch>
+            <Route
+                exact
+                path="/login"
+                render={({ history }) => <LoginPage history={history} />}
+            />
+            <Route
+                exact
+                path="/signup"
+                render={({ history }) => <SignupPage history={history} />}
+            />
+            <Route exact path="/" render={() => <HomePage />} />
+            <Route render={() => <Redirect to={{ pathname: '/login' }} />} />
+        </Switch>
+    );
+
     return (
         <div className="App">
             <Navbar history={props.history} />
-            <main>
-                <Switch>
-                    <Route exact path="/" render={() => <HomePage />} />
-                    <Route
-                        exact
-                        path="/new"
-                        render={({ history }) => (
-                            <NewApplicationPage history={history} />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path="/login"
-                        render={({ history }) => (
-                            <LoginPage history={history} />
-                        )}
-                    />
-                    <Route
-                        exact
-                        path="/signup"
-                        render={({ history }) => (
-                            <SignupPage history={history} />
-                        )}
-                    />
-                    <Route render={() => <Redirect to={{ pathname: '/' }} />} />
-                </Switch>
-            </main>
+            <main>{pages}</main>
             <footer>
                 <div>Footer</div>
             </footer>
